586cb9e57ce51dd402ff01a92c5908ff4ab7d9ab
Katarzyna Kadyszewska <kadyszewskak@gmail.com>
2025-06-11 17:45:35 +0200

    feat: Implement recursive discovery of frequent itemsets

--- src/eclat.py
+++ src/eclat.py
@@ -35,9 +35,9 @@ class Eclat:
         self._build_initial_tid_lists()
         
         # Krok 2: Rekurencyjnie odkrywaj częste zbiory
+        self._discover_frequent_itemsets(self.tid_lists, k=1)
         
-        
-        print("Algorytm Eclat (fit) - do implementacji!")
+        print("Algorytm Eclat zakończył działanie.")
         return self
 
     def _build_initial_tid_lists(self):
@@ -83,3 +83,62 @@ class Eclat:
         Metoda zwracająca odkryte częste zbiory.
         """
         return self.frequent_itemsets
+    
+    def _discover_frequent_itemsets(self, item_tid_lists_k, k):
+        """
+        Rekurencyjnie odkrywa częste zbiory o rozmiarze k+1 na podstawie
+        zbiorów o rozmiarze k.
+
+        Args:
+            item_tid_lists_k (dict): Słownik {item: tid_list} dla zbiorów o rozmiarze k.
+                                     Dla k=1, item jest stringiem. Dla k>1, item jest frozensetem.
+            k (int): Aktualny rozmiar itemsetów.
+        """
+        # Warunek stopu rekurencji: jeśli nie ma już itemsetów do rozszerzenia
+        if not item_tid_lists_k:
+            return
+
+        min_transactions = self.min_support * self.num_transactions
+        
+        # Słownik na znalezione częste zbiory o rozmiarze k+1
+        frequent_itemsets_k_plus_1 = {}
+
+        # Używamy posortowanej listy kluczy, aby uniknąć duplikatów i powtórzeń
+        # Np. połączymy {A} z {B}, ale nie połączymy później {B} z {A}
+        items_k = sorted(list(item_tid_lists_k.keys()))
+
+        for i in range(len(items_k)):
+            for j in range(i + 1, len(items_k)):
+                item_i = items_k[i]
+                item_j = items_k[j]
+
+                # Łączymy dwa itemsety w nowego kandydata
+                # Dla k=1, łączymy dwa itemy w zbiór 2-elementowy
+                # Dla k>1, łączymy dwa zbiory k-elementowe
+                if k == 1:
+                    candidate = frozenset([item_i, item_j])
+                else: # k > 1
+                    candidate = item_i.union(item_j)
+
+                # Optymalizacja: jeśli kandydat ma zły rozmiar, pomijamy go
+                # To się dzieje, gdy itemsety nie miały wspólnego prefixu
+                if len(candidate) != k + 1:
+                    continue
+                
+                # Obliczamy TID-listę kandydata przez przecięcie TID-list jego składowych
+                tid_list_i = item_tid_lists_k[item_i]
+                tid_list_j = item_tid_lists_k[item_j]
+                candidate_tid_list = tid_list_i.intersection(tid_list_j) #serce Eclat - zamiast skanowac przecinamy dwa zbiory ID transakcji, szybkie
+
+                # Sprawdzamy, czy kandydat jest częsty
+                if len(candidate_tid_list) >= min_transactions:
+                    frequent_itemsets_k_plus_1[candidate] = candidate_tid_list
+
+        # Jeśli znaleziono jakieś częste zbiory, dodajemy je do głównego wyniku
+        if frequent_itemsets_k_plus_1:
+            # Aktualizujemy nasz główny słownik z wynikami
+            for itemset, tid_list in frequent_itemsets_k_plus_1.items():
+                self.frequent_itemsets[itemset] = len(tid_list)
+            
+            # Wywołujemy rekurencję dla nowo znalezionych zbiorów
+            self._discover_frequent_itemsets(frequent_itemsets_k_plus_1, k + 1)
